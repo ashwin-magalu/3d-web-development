39ebab6a4f1b739c12ce8b04605f8e7a9fa40f2c
Ashwin M A <ashwin.magalu@gmail.com>
2024-01-27 13:30:43 +0530

    First Project final commit

--- first-threejs-project/src/script.js
+++ first-threejs-project/src/script.js
@@ -1,39 +1,43 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+// Getting Canvas tag from the HTML document
+const canvas = document.querySelector("canvas.webgl");
 
-// Scene
-const scene = new THREE.Scene()
+// Creating a Scene
+const scene = new THREE.Scene();
 
 /**
- * Object
+ * creating and adding an Object to the Scene with the help of Mesh. Mesh is a combination of
+ * Geometry (shape) and Material (how it looks). here Mesh will be the object which we can see
+ * in the Scene. Geometry represents the shape, while Material defines how that shape appears.
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 /**
- * Camera
+ * Adding a Camera, to see the scene from the point of view you like, We can have multiple cameras as
+ * well, but generally we use a single camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera); // adding camera to the scene is optional, but add it to the scene to avoid bugs
 
 /**
- * Renderer
+ * Renderer - The renderer will render the scene from the camera's point of view. The result will be
+ * drawn into a canvas
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
