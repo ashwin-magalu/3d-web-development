1c1908db9962b21ba5fe1d25b4af52956722d580
Ashwin M A <ashwin.magalu@gmail.com>
2024-01-28 20:28:57 +0530

    Chapter 7: debug ui

---
+++ 1-7-debug-ui/src/script.js
@@ -0,0 +1,167 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import gsap from "gsap";
+import GUI from "lil-gui";
+
+/**
+ * Debug
+ */
+// Normal GUI setup
+// const gui = new GUI();
+// Enhanced GUI setup
+const gui = new GUI({
+  width: 300,
+  title: "GUI control panel",
+  //   closeFolders: true, // by default it will be false. If true, the control panel will be collapsed
+});
+// gui.close(); // This will keep the gui panel collapsed by default, this is optional
+// gui.hide(); // will hide the gui panel by default
+
+// Unhide / hide GUI on pressing 'h' / 'H' key
+window.addEventListener("keydown", (e) => {
+  if (e.key === "h" || e.key === "H") {
+    gui.show(gui._hidden);
+  }
+});
+
+const debugObject = {};
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Object
+ */
+debugObject.color = "#3a6ea6";
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: debugObject.color });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Folders
+const cubeTweaks = gui.addFolder("My cube controls");
+// cubeTweaks.close(); // This will keep the folder collapsed by default, this is optional
+
+// Example for range
+// 2nd to 4th params are optional. They mean min value, max value and the step
+// gui.add(mesh.position, "y", -3, 3, 0.01);
+// gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
+cubeTweaks.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation"); // adding this to My cube controls folder
+
+// Example for Checkbox
+// gui.add(mesh, "visible");
+cubeTweaks.add(mesh, "visible");
+// gui.add(material, "wireframe"); // mesh.material can also be used here
+cubeTweaks.add(material, "wireframe");
+
+// Example for Color
+// gui.addColor(material, "color"); // mesh.material can also be used here
+// Retrieving the modified color
+// Solution 1
+// gui.addColor(material, "color").onChange((value) => {
+//   console.log("color changed to: ", value.getHexString());
+// });
+// Solution 2
+gui.addColor(debugObject, "color").onChange(() => {
+  material.color.set(debugObject.color);
+});
+
+// Example for Function and Button
+debugObject.spin = () => {
+  gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 });
+};
+gui.add(debugObject, "spin");
+
+debugObject.subdivision = 2;
+gui
+  .add(debugObject, "subdivision")
+  .min(1)
+  .max(20)
+  .step(1)
+  .onFinishChange(() => {
+    mesh.geometry.dispose();
+    mesh.geometry = new THREE.BoxGeometry(
+      1,
+      1,
+      1,
+      debugObject.subdivision,
+      debugObject.subdivision,
+      debugObject.subdivision
+    );
+  });
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # react-three-fiber-course
 
-ThreeJS Course project code in the chapter wise order
+3D Web Development Course project code in the chapter wise order
 
 # Documentation Link:
 
-[Google Doc Notes Link for Three.JS](https://docs.google.com/document/d/1cbYKzyJrq4JqL8Tk5rjSDGIttiwAn5cZaFUe6CwkDX0/edit#heading=h.bjqnsysl2k3s)
+[Google Doc Notes Link for 3D Web Development](https://docs.google.com/document/d/1cbYKzyJrq4JqL8Tk5rjSDGIttiwAn5cZaFUe6CwkDX0/edit#heading=h.bjqnsysl2k3s)
